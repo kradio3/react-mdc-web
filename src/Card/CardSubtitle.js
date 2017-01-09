4bf15e60b03356e9a32c859e9476332aca9f736e
Andrey.Yaroshenko <kradio3@gmail.com>
2017-01-09 14:27:05 +0300

    feat(CardSubtitle): create new component

---
+++ src/Card/CardSubtitle.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+const propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+const CardSubtitle = ({ className, children }) => (
+  <h2
+    className={classnames('mdc-card__subtitle', className)}
+  >
+    {children}
+  </h2>
+  );
+CardSubtitle.propTypes = propTypes;
+export default CardSubtitle;
