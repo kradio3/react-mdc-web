5a05d3994ddf3278f276adf62b392b249f7b78dc
Andrey.Yaroshenko <kradio3@gmail.com>
2017-01-25 11:17:24 +0300

    feat(List): create ListDivider component

---
+++ src/List/ListDivider.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+const propTypes = {
+  className: PropTypes.string,
+  inset: PropTypes.bool,
+};
+
+const ListDivider = ({ className, inset }) => (
+  <li
+    className={classnames('mdc-list-divider', {
+      'mdc-list-divider--inset': inset,
+    }, className)}
+  />
+  );
+ListDivider.propTypes = propTypes;
+export default ListDivider;
--- src/List/index.js
+++ src/List/index.js
@@ -2,3 +2,4 @@ import '@material/list/dist/mdc.list.min.css';
 
 export { default } from './List';
 export { ListItem } from './ListItem';
+export { ListDivider } from './ListDivider';
