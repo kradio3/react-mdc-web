cbf86f779ec41fa242ec7347bbb0af809d987a73
bw2012 <demondark20@gmail.com>
2017-02-09 13:31:41 +0300

    feat(Cell): tablet and phone prpos was added

--- src/LayoutGrid/Cell.js
+++ src/LayoutGrid/Cell.js
@@ -5,14 +5,18 @@ const propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
   col: PropTypes.number,
+  tablet: PropTypes.number,
+  phone: PropTypes.number,
 };
 
-const Cell = ({ className, children, col, name, ...otherProps }) => {
+const Cell = ({ className, children, col, tablet, phone, name, ...otherProps }) => {
   console.log(children);
   return (
   <div
     className={classnames('mdc-layout-grid__cell', className, {
-    [`mdc-layout-grid__cell--span-${col}`]: col })}
+      [`mdc-layout-grid__cell--span-${col}`]: col,
+      [`mdc-layout-grid__cell--span-${tablet}-tablet`]: tablet,
+      [`mdc-layout-grid__cell--span-${phone}-phone`]: phone})}
     {...otherProps}
   >
     {children}
