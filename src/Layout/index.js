2425a64f4c3c2ccb038e9cc0e0612794644c6a8c
Andrey.Yaroshenko <kradio3@gmail.com>
2017-02-26 23:28:45 +0300

    feat(Layout): create new component

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,3 +2,4 @@ import '@material/layout-grid/dist/mdc.layout-grid.min.css';
 
 export { default as Grid } from './Grid';
 export { default as Cell } from './Cell';
+export { default as Layout } from './Layout';
