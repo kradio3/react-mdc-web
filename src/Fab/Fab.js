eba85182755c216acc739dcddc51b027ce486eab
Andrey.Yaroshenko <kradio3@gmail.com>
2017-03-02 17:59:18 +0300

    fix(Fab): correct icon position

--- src/Fab/Fab.js
+++ src/Fab/Fab.js
@@ -1,33 +1,43 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 
-const propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-  mini: PropTypes.bool,
-  plain: PropTypes.bool,
-};
 
-const Fab = ({
-  className,
-  children,
-  mini,
-  plain,
-  ...otherProps
-}) => {
-  const classes = classnames(
-    'mdc-fab', {
-      'mdc-fab--mini': mini,
-      'mdc-fab--plain': plain,
-    }, className);
-  return (
-    <button
-      className={classes}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-};
-Fab.propTypes = propTypes;
+class Fab extends React.PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    mini: PropTypes.bool,
+    plain: PropTypes.bool,
+  };
+
+  render() {
+    const {
+      className,
+      children,
+      mini,
+      plain,
+      ...otherProps
+    } = this.props;
+    const classes = classnames(
+      'mdc-fab', {
+        'mdc-fab--mini': mini,
+        'mdc-fab--plain': plain,
+      }, className);
+    return (
+      <button
+        className={classes}
+        ref={(native) => {
+          if (native) {
+            const icon = native.querySelector('i');
+            icon.classList.add('mdc-fab__icon');
+          }
+        }}
+        {...otherProps}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
 export default Fab;
