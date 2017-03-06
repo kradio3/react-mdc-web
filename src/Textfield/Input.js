7150149efe4b2b19ee3718f6b3ea2b5aa042b32b
Andrey.Yaroshenko <kradio3@gmail.com>
2017-03-06 10:00:12 +0300

    refactor(Textfield): remove unused event listeners

--- src/Textfield/Input.js
+++ src/Textfield/Input.js
@@ -1,38 +1,18 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 
-class Input extends React.PureComponent {
+const ROOT = 'mdc-textfield__input';
+const propTypes = {
+  disabled: PropTypes.bool,
+};
 
-  static ROOT = 'mdc-textfield__input';
-
-  static propTypes = {
-    disabled: PropTypes.bool,
-    onBlur: PropTypes.func,
-    onFocus: PropTypes.func,
-  }
-
-  constructor(props) {
-    super(props);
-    this.registerListeners = this.registerListeners.bind(this);
-  }
-
-  registerListeners(nativeInput) {
-    const { onFocus, onBlur } = this.props;
-    nativeInput.addEventListener('focus', onFocus);
-    nativeInput.addEventListener('blur', onBlur);
-  }
-
-  render() {
-    const { disabled, ...otherProps } = this.props;
-    return (
-      <input
-        className={classnames(Input.ROOT)}
-        disabled={disabled}
-        type="text"
-        ref={this.registerListeners}
-        {...otherProps}
-      />
-    );
-  }
-}
+const Input = ({ disabled, ...otherProps }) => (
+  <input
+    className={classnames(ROOT)}
+    disabled={disabled}
+    type="text"
+    {...otherProps}
+  />
+);
+Input.propTypes = propTypes;
 export default Input;
