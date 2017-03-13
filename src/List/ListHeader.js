f40347a812905d891fe88d193688d76244d2fa51
Andrey.Yaroshenko <kradio3@gmail.com>
2017-03-13 16:38:08 +0300

    feat(ListHeader): create new component

---
+++ src/List/ListHeader.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+
+const propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  component: PropTypes.string,
+};
+
+const defaultProps = {
+  component: 'h3',
+};
+
+const ListHeader = ({
+  className,
+  children,
+  component,
+  ...otherProps
+}) => {
+  const classes = classnames('mdc-list-group__subheader', className);
+  return React.createElement(component, {
+    className: classes,
+    ...otherProps,
+  }, children);
+};
+ListHeader.propTypes = propTypes;
+ListHeader.defaultProps = defaultProps;
+export default ListHeader;
+
--- src/List/index.js
+++ src/List/index.js
@@ -4,3 +4,4 @@ export { default as List } from './List';
 export { default as ListItem } from './ListItem';
 export { default as ListDivider } from './ListDivider';
 export { default as ListGroup } from './ListGroup';
+export { default as ListHeader } from './ListHeader';
