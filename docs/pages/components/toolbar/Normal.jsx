64323541ca03326f8c269d9035f505dfa84b86bd
bw2012 <demondark20@gmail.com>
2017-04-04 17:33:59 +0400

    feat(Toolbar): create ToolbarRow component (#16)
    * feat(Toolbar): add ToolbarRow
    
    * fix(Toolbar): fix lint errors
    
    * chore(deps): upgrade material-components-web
    
    * docs(Toolbar): ToolbarRow usage

--- docs/pages/components/toolbar/Normal.jsx
+++ docs/pages/components/toolbar/Normal.jsx
@@ -2,19 +2,22 @@ import React, {PropTypes} from 'react'
 import Toolbar from '../../../../src/Toolbar/Toolbar'
 import ToolbarSection from '../../../../src/Toolbar/ToolbarSection'
 import ToolbarTitle from '../../../../src/Toolbar/ToolbarTitle'
+import ToolbarRow from '../../../../src/Toolbar/ToolbarRow'
 
 class Normal extends React.PureComponent {
 
   render () {
     return (
-          <div style={{height: '300px', width: '500px', position: 'relative', overflow: 'auto'}}>
-            <Toolbar>
-              <ToolbarSection align="start">
-                <ToolbarTitle>Title</ToolbarTitle>
-              </ToolbarSection>
-            </Toolbar>
-            <div style={{height: '300px'}}>Content</div> 
-          </div>
+      <div style={{height: '300px', width: '500px', position: 'relative', overflow: 'auto'}}>
+        <Toolbar>
+          <ToolbarRow>
+            <ToolbarSection align="start">
+              <ToolbarTitle>Title</ToolbarTitle>
+            </ToolbarSection>
+          </ToolbarRow>
+        </Toolbar>
+        <div style={{height: '300px'}}>Content</div> 
+      </div>
     )
   }
 }
--- src/Toolbar/index.js
+++ src/Toolbar/index.js
@@ -1,3 +1,4 @@
 export { default as Toolbar } from './Toolbar';
 export { default as ToolbarSection } from './ToolbarSection';
 export { default as ToolbarTitle } from './ToolbarTitle';
+export { default as ToolbarRow } from './ToolbarRow';
