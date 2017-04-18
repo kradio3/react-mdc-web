1cc2357d08b6a86bc1935260911a143fe333bae2
Andrey.Yaroshenko <kradio3@gmail.com>
2017-04-18 16:41:23 +0300

    chore: sitemap generator

--- docs/gatsby-node.js
+++ docs/gatsby-node.js
@@ -1,6 +1,9 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+//var sm = require('sitemap');
+import fs from 'fs-extra-promise';
+import sitemap from 'sitemap';
 
-exports.modifyWebpackConfig = function(config, stage) {
+export const modifyWebpackConfig = (config, stage) => {
   config.removeLoader('css');
   if(stage === 'develop') {
     config.loader('css', function(cfg) {
@@ -23,4 +26,36 @@ exports.modifyWebpackConfig = function(config, stage) {
     ["css/styles.css", { allChunks: true }]
   );
   return config;
+}
+
+const pagesToSitemap = pages => {
+  const urls = pages.map((p) => {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'weekly',
+        priority: 0.5,
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(u => u !== undefined)
+}
+
+const generateSiteMap = pages => {
+  const map = sitemap.createSitemap({
+    hostname: 'https://kradio3.github.io/react-mdc-web',
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    map.toString()
+  )
+}
+
+export const postBuild = (pages,callback) => {
+  generateSiteMap(pages);
+  callback();
 }
