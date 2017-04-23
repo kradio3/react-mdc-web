a69552cf981f982a8f34b849c5b11bd0538176f1
Andrey.Yaroshenko <kradio3@gmail.com>
2017-04-23 09:26:35 +0300

    fix(Menu): remove css import (#21)

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,5 +1,3 @@
-import '@material/menu/dist/mdc.menu.min.css';
-
 export { default as Menu } from './Menu';
 export { default as MenuItem } from './MenuItem';
 export { default as MenuDivider } from './MenuDivider';
